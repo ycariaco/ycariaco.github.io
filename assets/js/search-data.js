6656dca5e69f3124a39679c536e21942bc380d2e
ycariaco <yusmaris_c@hotmail.com>
2026-03-04 15:07:32 +0000

    Deploying to gh-pages from @ ycariaco/ycariaco.github.io@caf5862469d724cec71e844ce5bf6b65e151d36d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-welcome",
-          title: "Welcome",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",

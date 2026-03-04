3a64930a927cc5f301f4da8dae4996dac20e8649
ycariaco <yusmaris_c@hotmail.com>
2026-03-04 05:29:24 +0000

    Deploying to gh-pages from @ ycariaco/ycariaco.github.io@11898fa09ae4a7aa7abb1959505e48f2c4da2f79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,14 +3,14 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-welcome",
-    title: "Welcome",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-welcome",
-          title: "Welcome",
+  },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {

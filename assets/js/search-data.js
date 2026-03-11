125eefe1cef391c7283ad2759c08861c161fb06c
ycariaco <yusmaris_c@hotmail.com>
2026-03-11 05:09:00 +0000

    Deploying to gh-pages from @ ycariaco/ycariaco.github.io@6e5acee8b1d5ad6e890a5c4dd40701d8832af017 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-cariaco-lab-was-created",
+          title: 'Cariaco lab was created!',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',

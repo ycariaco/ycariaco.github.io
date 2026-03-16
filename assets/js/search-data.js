730f9aadcd1a452843c5ba7fd8dfdc64f2ba9898
ycariaco <yusmaris_c@hotmail.com>
2026-03-16 03:38:20 +0000

    Deploying to gh-pages from @ ycariaco/ycariaco.github.io@17a81c636d0e7b4263c0cf110ff99de56bd54860 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-plexo",
+          title: "PlExO",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/plexo/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",

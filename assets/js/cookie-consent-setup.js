78eb9c0298ccbfa1bccef3f88adc5f5175fa2d80
ycariaco <yusmaris_c@hotmail.com>
2026-04-01 19:14:37 +0000

    Deploying to gh-pages from @ ycariaco/ycariaco.github.io@5c6617c68aed782e225572145a62b5c241f49146 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://ycariaco.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://cariacolab.com/#contact">contact us</a>.'
               }
             ]
           }
